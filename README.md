e72ddfbf5da64f704129cd29f8ff913576b3b8ae
curtis <curtis.vermeeren@gmail.com>
2017-01-30 15:16:23 -0500

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+To start the server type `node hello_server.js`
+
+The server is attempted to be bound on port 8080 so if this port is already in
+use you will receiver an error.
+
+You can edit `server.listen(8080);` to a different port.
+
+Navigate to either `http://localhost:8080` or `http://127.0.0.1:8080` to see the
+results of the server. Hello World! should be printed. 
---
+++ hello_server.js
@@ -0,0 +1,21 @@
+// Load the http module
+var http = require('http');
+
+// Create a new HTTP server
+// This createServer function takes a callback function which is run when a request is received
+// request has information about the client's request, response is used to return data.
+var server = http.createServer(function(request, response){
+	// Write 200 to the http head. 200 is the status code for 'OK'
+	// This serve specifies the Content-Type header
+	response.writeHead(200, {"Content-Type": "text/html"});
+	//Write to the HTML page Hello World!
+	response.write(`<!DOCTYPE "html"><html><head><title>Hello World Page</title>`+
+		`</head><body>Hello World!</body></html>`);
+    // When we call end() we tell the serve the response headers and body have been sent.
+	// End can also be used like write assuming only one call is needed. 
+	response.end();
+});
+
+// Listen on port 8080
+server.listen(8080);
+console.log("Server is Listening");
